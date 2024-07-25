e4db8af5024a8569a70084c50c04930d2d2e87ec
ADITI SHARMA <aditisharma98574@gmail.com>
2024-07-25 16:07:04 +0530

    initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Marquee from "react-fast-marquee";
+
+function Header() {
+  return (
+    <>
+    <div className='w-full h-10 bg-[#ffebe9] pl-4 pt-2'>
+    <Marquee>
+     Header
+    </Marquee>
+    </div>
+    </>
+  )
+}
+
+export default Header
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,89 @@
+import React, { useRef } from 'react';
+import { Bounce, toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { MdEmail, MdHome, MdWork } from 'react-icons/md';
+import { RiFileInfoFill } from 'react-icons/ri';
+
+const Navbar = () => {
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const servicesRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const handleNotification = (message, ref) => {
+    const rect = ref.current.getBoundingClientRect();
+    toast(message, {
+      position: toast.POSITION.TOP_RIGHT,
+      autoClose: false,
+      transition: Bounce,
+      closeOnClick: false,
+      style: {
+        position: 'fixed',
+        top: rect.bottom + window.scrollY + 10, 
+        left: rect.left + window.scrollX,
+        transform: 'translateY(0)',
+        width: '500px', 
+        textAlign: 'center', 
+      },
+    });
+  };
+
+  return (
+    <>
+      <nav className="bg-white p-4 shadow-md">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="text-[#8e160b] text-2xl font-bold">Some logo</div>
+          <div className="flex mx-auto justify-between items-center space-x-16">
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault(); // Prevent default behavior
+                handleNotification('This is Home', homeRef);
+              }}
+              ref={homeRef}
+              className="text-[#8e160b] hover:text-black"
+            >
+              <MdHome size={26} />
+            </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                handleNotification('This is About', aboutRef);
+              }}
+              ref={aboutRef}
+              className="text-[#8e160b] hover:text-black"
+            >
+              <RiFileInfoFill size={24} />
+            </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                handleNotification('This is Services', servicesRef);
+              }}
+              ref={servicesRef}
+              className="text-[#8e160b] hover:text-black "
+            >
+              <MdWork size={24} />
+            </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                handleNotification('This is Contact', contactRef);
+              }}
+              ref={contactRef}
+              className="text-[#8e160b] hover:text-black"
+            >
+              <MdEmail size={24} />
+            </a>
+          </div>
+        </div>
+      </nav>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default Navbar;
